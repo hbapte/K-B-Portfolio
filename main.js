b6a447fb850fac9e502898e1f1fdb58f3a4ca21e
hbapte <ijbapte@gmail.com>
2023-11-19 18:48:04 +0200

    Add dark mode and smooth scrolling

--- main.js
+++ main.js
@@ -1,72 +1,107 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
 
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }
-  
-  // Whenever the user explicitly chooses light mode
-  localStorage.theme = 'light'
-  
-  // Whenever the user explicitly chooses dark mode
-  localStorage.theme = 'dark'
-  
-  // Whenever the user explicitly chooses to respect the OS preference
-  localStorage.removeItem('theme')
+// Nav 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav a");
 
-  
-  
-  
-  
-  
-  const swiper = new Swiper('.swiper', {
-    speed: 400,
-    loop:true,
-    spaceBetween: 30,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true,
-    },
+  function onScroll() {
+    const scrollPosition = window.scrollY;
 
-    navigation:{
-        prevEl: ".prev-button",
-        nextEl: ".next-button",
-       
-      }, 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-
-   
-
-  
-
-    grabCursor: true,
-    breakpoints: {
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 1,
-      },
-      // when window width is >= 768px
-      768: {
-        slidesPerView: 2,
-      },
-      // when window width is >= 1024px
-      1024: {
-        slidesPerView: 2,
+    navLinks.forEach((link) => {
+      const targetSection = document.querySelector(link.getAttribute("href"));
+      if (
+        targetSection.offsetTop <= scrollPosition &&
+        targetSection.offsetTop + targetSection.offsetHeight > scrollPosition
+      ) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
       }
-    }
-  });
+    });
+  }
+
+  window.addEventListener("scroll", onScroll);
+});
 
 
-  /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+
+
+
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+// Check the initial theme mode
+if (localStorage.getItem("mode") === "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+// Set the theme on button click
+themeBtn.addEventListener("click", (e) => {
+  if (html.classList.contains("dark")) {
+    lightMode();
+  } else {
+    darkMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
+}
+
+
+
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  loop: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  navigation: {
+    prevEl: ".prev-button",
+    nextEl: ".next-button",
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  grabCursor: true,
+  breakpoints: {
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+/*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 const closeIcon = document.getElementById("nav-close");
@@ -86,101 +121,28 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.remove("hidden");
 });
 
-
- 
-
 // TABS
 function showContent(tabName, button) {
-    // Hide all content sections
-    document.getElementById('designContent').style.display = 'none';
-    document.getElementById('developmentContent').style.display = 'none';
-    document.getElementById('marketingContent').style.display = 'none';
+  // Hide all content sections
+  document.getElementById("designContent").style.display = "none";
+  document.getElementById("developmentContent").style.display = "none";
+  document.getElementById("marketingContent").style.display = "none";
 
-    // Remove the active-tab class from all buttons
-    var buttons = document.querySelectorAll('#tabButtons button');
-    buttons.forEach(function (btn) {
-      btn.classList.remove('active-tab');
-    });
-
-    // Show the selected content section
-    document.getElementById(tabName + 'Content').style.display = 'grid';
-
-    // Add the active-tab class to the clicked button
-    button.classList.add('active-tab');
-  }
-
-
-  
-/*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
-const tabs = document.querySelectorAll(".tabs_wrap ul li");
-const all = document.querySelectorAll(".item_wrap");
-const foods = document.querySelectorAll(".food");
-const beverages = document.querySelectorAll(".beverage");
-const snacks = document.querySelectorAll(".snack");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
-
-    tab.classList.add("active");
-
-    const tabval = tab.getAttribute("data-tabs");
-
-    all.forEach((item) => {
-      item.style.display = "none";
-    });
-
-    if (tabval == "food") {
-      foods.forEach((item) => {
-        item.style.display = "block";
-      });
-    } else if (tabval == "snack") {
-      snacks.forEach((item) => {
-        item.style.display = "block";
-      });
-    } else if (tabval == "beverage") {
-      beverages.forEach((item) => {
-        item.style.display = "block";
-      });
-    } else {
-      all.forEach((item) => {
-        item.style.display = "block";
-      });
-    }
+  // Remove the active-tab class from all buttons
+  var buttons = document.querySelectorAll("#tabButtons button");
+  buttons.forEach(function (btn) {
+    btn.classList.remove("active-tab");
   });
-});
 
-/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
-const html = document.querySelector("html");
-const themeBtn = document.getElementById("theme-toggle");
+  // Show the selected content section
+  document.getElementById(tabName + "Content").style.display = "grid";
 
-if (localStorage.getItem("mode") == "dark") {
-  darkMode();
-} else {
-  lightMode();
+  // Add the active-tab class to the clicked button
+  button.classList.add("active-tab");
 }
 
-themeBtn.addEventListener("click", (e) => {
-  if (localStorage.getItem("mode") == "light") {
-    darkMode();
-  } else {
-    lightMode();
-  }
-});
 
-function darkMode() {
-  html.classList.add("dark");
-  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
-  localStorage.setItem("mode", "dark");
-}
 
-function lightMode() {
-  html.classList.remove("dark");
-  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
-  localStorage.setItem("mode", "light");
-}
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
 const scrollUp = () => {
@@ -195,6 +157,9 @@ const scrollUp = () => {
   }
 };
 window.addEventListener("scroll", scrollUp);
+
+
+
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
 const scrollHeader = () => {
@@ -231,30 +196,29 @@ const activeLink = () => {
   });
 };
 
-window.addEventListener('scroll', activeLink)
+window.addEventListener("scroll", activeLink);
 
 /*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
 const sr = ScrollReveal({
   origin: "top",
   distance: "60px",
   duration: 2500,
-  delay: 400
-})
+  delay: 400,
+});
 
 sr.reveal(".home__image");
-sr.reveal(".home__content",  { origin: "bottom" });
+sr.reveal(".home__content", { origin: "bottom" });
 
-sr.reveal(".category__card", { interval: 300 })
+sr.reveal(".category__card", { interval: 300 });
 
-sr.reveal(".promo__card-1",  { origin: "left" });
-sr.reveal(".promo__card-2",  { origin: "right" });
+sr.reveal(".promo__card-1", { origin: "left" });
+sr.reveal(".promo__card-2", { origin: "right" });
 
-sr.reveal(".about__img",  { origin: "bottom" });
-sr.reveal(".about__content",  { origin: "top" });
+sr.reveal(".about__img", { origin: "bottom" });
+sr.reveal(".about__content", { origin: "top" });
 
-sr.reveal(".menu__items",  { origin: "left" });
+sr.reveal(".menu__items", { origin: "left" });
 
-sr.reveal(".customer__review",  { origin: "right" });
+sr.reveal(".customer__review", { origin: "right" });
 
 sr.reveal(".footer");
-
