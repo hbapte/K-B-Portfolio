99e6b79bced9d6ea6ce5d73699c8c7cd1d841631
hbapte <ijbapte@gmail.com>
2023-11-19 02:17:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,260 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+  
+  // Whenever the user explicitly chooses light mode
+  localStorage.theme = 'light'
+  
+  // Whenever the user explicitly chooses dark mode
+  localStorage.theme = 'dark'
+  
+  // Whenever the user explicitly chooses to respect the OS preference
+  localStorage.removeItem('theme')
+
+
+
+  
+  
+  
+  
+  
+  const swiper = new Swiper('.swiper', {
+    speed: 400,
+    loop:true,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+
+    navigation:{
+        prevEl: ".prev-button",
+        nextEl: ".next-button",
+       
+      }, 
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+
+   
+
+  
+
+    grabCursor: true,
+    breakpoints: {
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 1024px
+      1024: {
+        slidesPerView: 2,
+      }
+    }
+  });
+
+
+  /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+const closeIcon = document.getElementById("nav-close");
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    navMenu.classList.add("hidden");
+  })
+);
+
+closeIcon.addEventListener("click", () => {
+  navMenu.classList.add("hidden");
+});
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.remove("hidden");
+});
+
+
+ 
+
+// TABS
+function showContent(tabName, button) {
+    // Hide all content sections
+    document.getElementById('designContent').style.display = 'none';
+    document.getElementById('developmentContent').style.display = 'none';
+    document.getElementById('marketingContent').style.display = 'none';
+
+    // Remove the active-tab class from all buttons
+    var buttons = document.querySelectorAll('#tabButtons button');
+    buttons.forEach(function (btn) {
+      btn.classList.remove('active-tab');
+    });
+
+    // Show the selected content section
+    document.getElementById(tabName + 'Content').style.display = 'grid';
+
+    // Add the active-tab class to the clicked button
+    button.classList.add('active-tab');
+  }
+
+
+  
+/*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
+const tabs = document.querySelectorAll(".tabs_wrap ul li");
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const beverages = document.querySelectorAll(".beverage");
+const snacks = document.querySelectorAll(".snack");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    tab.classList.add("active");
+
+    const tabval = tab.getAttribute("data-tabs");
+
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    if (tabval == "food") {
+      foods.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "snack") {
+      snacks.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "beverage") {
+      beverages.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else {
+      all.forEach((item) => {
+        item.style.display = "block";
+      });
+    }
+  });
+});
+
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("mode") == "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+  if (localStorage.getItem("mode") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
+}
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-4");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-4");
+  }
+};
+window.addEventListener("scroll", scrollUp);
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 50) {
+    header.classList.add("border-b", "border-secondaryColor");
+  } else {
+    header.classList.remove("border-b", "border-secondaryColor");
+  }
+};
+window.addEventListener("scroll", scrollHeader);
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+const activeLink = () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav__link");
+
+  let current = "home";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (this.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((item) => {
+    item.classList.remove("text-secondaryColor");
+    if (item.href.includes(current)) {
+      item.classList.add("text-secondaryColor");
+    }
+  });
+};
+
+window.addEventListener('scroll', activeLink)
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400
+})
+
+sr.reveal(".home__image");
+sr.reveal(".home__content",  { origin: "bottom" });
+
+sr.reveal(".category__card", { interval: 300 })
+
+sr.reveal(".promo__card-1",  { origin: "left" });
+sr.reveal(".promo__card-2",  { origin: "right" });
+
+sr.reveal(".about__img",  { origin: "bottom" });
+sr.reveal(".about__content",  { origin: "top" });
+
+sr.reveal(".menu__items",  { origin: "left" });
+
+sr.reveal(".customer__review",  { origin: "right" });
+
+sr.reveal(".footer");
+
